4e94c612b506e82f24519c8862631f187e40b603
zcarey <zcarey@R8401983>
2018-11-11 10:36:28 -0500

    First Working Stack

--- RobotArmUR2/RobotProgram.cs
+++ RobotArmUR2/RobotProgram.cs
@@ -30,25 +30,25 @@ namespace RobotArmUR2 {
 			float targetDistance = (1f - relativePaperCoords.Y) * percentD1 + relativePaperCoords.Y * percentD2;
 			*/
 			RobotCalibration calib = Robot.Calibration;
-			double x1 = calib.Distance1 * Math.Cos(calib.Angle1 * Math.PI / 180);
-			double x2 = calib.Distance2 * Math.Cos(calib.Angle2 * Math.PI / 180);
-			double x3 = calib.Distance3 * Math.Cos(calib.Angle3 * Math.PI / 180);
-			double x4 = calib.Distance4 * Math.Cos(calib.Angle4 * Math.PI / 180);
+			double x1 = calib.Distance1 * Math.Cos((180 - calib.Angle1) * Math.PI / 180);
+			double x2 = calib.Distance2 * Math.Cos((180 - calib.Angle2) * Math.PI / 180);
+			double x3 = calib.Distance3 * Math.Cos((180 - calib.Angle3) * Math.PI / 180);
+			double x4 = calib.Distance4 * Math.Cos((180 - calib.Angle4) * Math.PI / 180);
 
-			double y1 = calib.Distance1 * Math.Sin(calib.Angle1 * Math.PI / 180);
-			double y2 = calib.Distance2 * Math.Sin(calib.Angle2 * Math.PI / 180);
-			double y3 = calib.Distance3 * Math.Sin(calib.Angle3 * Math.PI / 180);
-			double y4 = calib.Distance4 * Math.Sin(calib.Angle4 * Math.PI / 180);
+			double y1 = calib.Distance1 * Math.Sin((180 - calib.Angle1) * Math.PI / 180);
+			double y2 = calib.Distance2 * Math.Sin((180 - calib.Angle2) * Math.PI / 180);
+			double y3 = calib.Distance3 * Math.Sin((180 - calib.Angle3) * Math.PI / 180);
+			double y4 = calib.Distance4 * Math.Sin((180 - calib.Angle4) * Math.PI / 180);
 
-			double alpha = relativePaperCoords.Y;
-			double beta = relativePaperCoords.X;
+			double alpha = relativePaperCoords.X;
+			double beta = relativePaperCoords.Y;
 
-			double x = alpha * beta * (x2 + x4 - x1 - x3) + alpha * (x1 - x2) + beta * (x3 - x2) + x2;
-			double y = alpha * beta * (y2 + y4 - y1 - y3) + alpha * (y1 - y2) + beta * (y3 - y2) + y2;
-			Console.WriteLine("Relative: [{0}, {1}]", x, y);
+			double x = alpha * (x3 - x2) + beta * (x1 - x2) + alpha * beta * (x4 + x2 - x1 - x3) + x2;
+			double y = alpha * (y3 - y2) + beta * (y1 - y2) + alpha * beta * (y4 + y2 - y1 - y3) + y2;
+			Console.WriteLine("Relative: [{0}, {1}]", x, y); //TODO remove
 
-			double targetAngle = Math.Abs(Math.Atan(y / x)) * 180 / Math.PI;
-			double targetDistance = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+			double targetAngle = 180 - (Math.Atan2(y, x) * 180 / Math.PI);
+			double targetDistance = Math.Sqrt(x * x + y * y);
 			Console.WriteLine("Target: [{0}, {1}mm]\n", targetAngle, targetDistance);
 
 			//Console.WriteLine("[{0}, {1}]", targetAngle, targetDistance);
